9b8562a6d7b3c742fa0e744d75fb10b670f00c7e
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2022-03-19 10:58:53 -0300

    Ultima atualização do projeto incompleto

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 
 
 
-function obterDados()
-{
-    let ano         = document.getElementById('ano').value
-    let mes         = document.getElementById('mes').value
-    let dia         = document.getElementById('dia').value
-    let tipo        = document.getElementById('tipo').value
-    let descricao   = document.getElementById('descricao').value
-    let valor       = document.getElementById('valor').value
+function obterDados() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
 
 
-    let BD = 
+
+
+    let BD =
     {
 
         ano,
@@ -20,63 +21,61 @@ function obterDados()
         tipo,
         descricao,
         valor,
-
-
-            registroBD: function (key, ano, mes, dia, tipo, descricao, valor)
-            {
-
-                ano = this.ano
-                mes = this.mes
-                dia = this.dia
-                tipo = this.tipo
-                descricao = this.descricao
-                valor = this.valor
-
-
-                localStorage.setItem(key, JSON.stringify(BD))
-                
-            }
+        total:0,
         
-    }
+        
 
-         
-    
-    for (i = 0; i <= localStorage.length ; i++)
-    {    
 
-       if (ano =="" || mes =="" || dia =="" || tipo =="" || descricao =="" || valor =="" )
-       {
-       
-       document.getElementById("botao").setAttribute("data-target", "#ModalFalha")
-           
-           break
+        registroBD: function (key) {
+
+            BD.total = key
+
+            localStorage.setItem(key, JSON.stringify(BD))
+ 
+            
 
         }
 
-        else{
+    }
 
-            if (localStorage.getItem(i) == null)
-            {            
-            
-                    BD.registroBD(i, ano,mes,dia,tipo,descricao,valor)
 
-                     ano         = document.getElementById('ano').value         =""
-                     mes         = document.getElementById('mes').value         =""
-                     dia         = document.getElementById('dia').value         =""
-                     tipo        = document.getElementById('tipo').value        =""
-                     descricao   = document.getElementById('descricao').value   =""
-                     valor       = document.getElementById('valor').value       =""
 
-                     document.getElementById("botao").setAttribute("data-target", "#ModalSucesso")
-                      
-                    break            
+    for (i = 0; i <= localStorage.length; i++) {
+
+        if (ano == "" || mes == "" || dia == "" || tipo == "" || descricao == "" || valor == "") {
+
+            document.getElementById("botao").setAttribute("data-target", "#ModalFalha")
+
+            break
+
+        }
+
+        else {
+
+            if (localStorage.getItem(i) == null) {
+
+                BD.registroBD(i)
+
+
+
+
+
+                ano = document.getElementById('ano').value = ""
+                mes = document.getElementById('mes').value = ""
+                dia = document.getElementById('dia').value = ""
+                tipo = document.getElementById('tipo').value = ""
+                descricao = document.getElementById('descricao').value = ""
+                valor = document.getElementById('valor').value = ""
+
+                document.getElementById("botao").setAttribute("data-target", "#ModalSucesso")
+
+                break
 
             }
-            
-            else
-                {
-                    continue 
-                }
+
+            else {
+                continue
+            }
         }
 
 
@@ -84,17 +83,6 @@ function obterDados()
 
 
     }
-
-}
-
-
-
-
-
-function registrar()
-{
-
-    
 
 }
 
@@ -103,65 +91,100 @@ function registrar()
 
 function criarTabela() {
 
-    for (i=0 ; i < localStorage.length ; i++){
 
-let Dados = JSON.parse(localStorage.getItem(i))
+   
 
-        switch (Dados.tipo)
-        {
 
-            case '1':
-                Dados.tipo = "Alimentação";
-                break;
+    for (i = 0; i <= 100; i++) {
+ 
+        let Dados = JSON.parse(localStorage.getItem(i))
                 
 
-            case '2':
-                Dados.tipo = "Educação";
-                break;
-                
+        if (Dados != null)
+         {
+            switch (Dados.tipo) {
 
-            case '3':
-                Dados.tipo = "Lazer";
-                break;
-                
+                case '1':
 
-            case '4':
-                Dados.tipo = "Saúde";
-                break;
-                
-            case '5':
-                Dados.tipo = "Transporte";
-                break;
-        
+                    Dados.tipo = "Alimentação";
+                    break;
+
+
+                case '2':
+                    Dados.tipo = "Educação";
+                    break;
+
+
+                case '3':
+                    Dados.tipo = "Lazer";
+                    break;
+
+
+                case '4':
+                    Dados.tipo = "Saúde";
+                    break;
+
+                case '5':
+                    Dados.tipo = "Transporte";
+                    break;
+
+            }
+
+
+
+            let removerDespesas = {
+
+               
+
+                remover: function(dados){
+
+                    
+                    console.log(dados)
+
+                }
+
+            }
+
+
+            let tabela = document.getElementById("tabela")
+
+            let nova_td = document.createElement("tbody")
+            nova_td.innerHTML =    `<td> ${Dados.dia}/${Dados.mes}/${Dados.ano}</td> 
+                                    <td> ${Dados.tipo}</td>
+                                    <td> ${Dados.descricao}</td>
+                                    <td> ${Dados.valor}</td>   
+                                    <td> <button type="button" id="${Dados.total}" class="btn btn-danger" onclick="${removerDespesas.remover(Dados.total)}">
+                                    <strong>X</strong>
+                                    </button> </td>`
+            
+            tabela.appendChild(nova_td)
+
+
+
+           
+            
+
         }
 
+        else {continue}
 
-   
+    }
 
-    let tabela = document.getElementById("tabela")
 
-    let nova_td = document.createElement("tbody")
-    nova_td.innerHTML = `<td>${Dados.dia}/${Dados.mes}/${Dados.ano}</td> 
-                         <td> ${Dados.tipo}</td>
-                         <td> ${Dados.descricao}</td>
-                         <td> ${Dados.valor}</td>
-    
-    
-    `
 
-    tabela.appendChild(nova_td)
+
 
 
 }
 
 
 
-    
-
-   
 
 
-}
+
+
+
+
 
 
 
